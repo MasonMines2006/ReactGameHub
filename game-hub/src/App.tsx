37319aa1a23d37f8c94d8e69c15e5df39dfae0d7
MasonMines2006 <masonmines2006@gmail.com>
2025-07-09 16:28:55 -0500

    Implemented a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,14 +1,29 @@
-import Homepage from "./components/Homepage";
-import Navbar from "./components/Navbar";
-import Topbar from "./components/Topbar";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
+  const showAside = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
   return (
-    <>
-      <Topbar />
-      <Navbar />
-      <Homepage />
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={showAside}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
